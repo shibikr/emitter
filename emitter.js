6d82d91c871848c698441f798dc6f8b34e8f8fed
shibik <shibik@INshibik.local>
2015-10-27 11:47:12 +0530

    new function flower added

--- emitter.js
+++ emitter.js
@@ -5,4 +5,18 @@ emitter.on('wish',function(){
 	console.log('Namaskaram');
 });
 
+emitter.once('wish',function(){
+	console.log('hello');
+});
+
+emitter.on('flower',function(){
+	console.log('Rose');
+});
+
+emitter.on('flower',function(){
+	console.log('marigold');
+});
+
 emitter.emit('wish');
+emitter.emit('wish');
+emitter.emit('flower');
